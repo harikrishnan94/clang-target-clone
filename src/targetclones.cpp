33a71fc316ef600d6cba4f9241ee134f12aafb2e
Harikrishnan <harikrishnan.prabakaran@zohocorp.com>
2020-08-18 17:54:48 +0530

    Implement target clone based on CPU uArch

--- src/targetclones.cpp
+++ src/targetclones.cpp
@@ -12,14 +12,13 @@
 
 #include <array>
 #include <string>
+#include <string_view>
 #include <tuple>
 #include <vector>
 
 #include <unistd.h>
 
 using namespace llvm;
-
-void Dump(Value &V) { V.print(outs()); }
 
 #define DEBUG_WAIT()                                                                               \
   do {                                                                                             \
@@ -28,45 +27,67 @@ void Dump(Value &V) { V.print(outs()); }
   } while (0)
 
 namespace {
+inline auto to_strref(std::string_view str) -> StringRef { return {str.data(), str.length()}; }
+
 #ifndef TARGET_CLONE_ANNOTATION
-const char *TARGET_CLONE = "targetclone";
+constexpr std::string_view TARGET_CLONE = "targetclone";
 #else
-const char *TARGET_CLONE = TARGET_CLONE_ANNOTATION;
+constexpr std::string_view TARGET_CLONE = TARGET_CLONE_ANNOTATION;
 #endif
 
-using FunctionSet = SmallPtrSet<Function *, 4>;
+enum class Vendor : unsigned {
+// NOLINTNEXTLINE
+#define X86_VENDOR(ENUM, STRING) ENUM,
+#include <llvm/Support/X86TargetParser.def>
+};
 
-enum class TargetFeatureKind { SSE4_2 = 0, AVX, AVX2, AVX512F, LAST };
+enum class CPU : unsigned {
+// NOLINTNEXTLINE
+#define X86_CPU_TYPE(ARCHNAME, ENUM) ENUM,
+#include <llvm/Support/X86TargetParser.def>
+};
 
-const char *TARGET_FEATURES = "target-features";
-constexpr auto TYPICAL_FEATURE_COUNT = 20;
+enum class TargetID : unsigned {
+  DUMMY, // Enum Value Cannot be 0. Required by `emitX86CpuIs`.
+// NOLINTNEXTLINE
+#define X86_CPU_SUBTYPE_COMPAT(ARCHNAME, ENUM, STR) ENUM,
+#include <llvm/Support/X86TargetParser.def>
+};
+struct Target {
+  TargetID ID;
+  std::string_view Name;
+};
+
+std::array TARGETS = {
+// NOLINTNEXTLINE
+#define X86_CPU_SUBTYPE_COMPAT(ARCHNAME, ENUM, STR) Target{TargetID::ENUM, STR},
+#include <llvm/Support/X86TargetParser.def>
+};
+
+constexpr auto NUM_TARGETS = TARGETS.size();
+constexpr std::string_view TARGET_CPU = "target-cpu";
+constexpr std::string_view TARGET_FEATURES = "target-features";
+constexpr auto TYPICAL_FEATURE_COUNT = 32;
 constexpr auto TYPICAL_FEATURES_LEN = 512;
 constexpr auto TEMP_STR_LEN = 128;
 
-struct TargetFeature {
-  StringRef Name;
-  std::array<StringRef, TYPICAL_FEATURE_COUNT> Value;
-};
+using TargetFunctionPair = std::pair<std::reference_wrapper<const Target>, Function *>;
 
-const std::array
-    // NOLINTNEXTLINE - Maintain this in sorted in reverse order.
-    KNOWN_FEATURES = {
-        TargetFeature{"avx512f",
-                      {"+avx", "+avx2", "+avx512f", "+f16c", "+fma", "+fxsr", "+mmx", "+popcnt",
-                       "+sse", "+sse2", "+sse3", "+sse4.1", "+sse4.2", "+ssse3", "+x87", "+xsave"}},
-        TargetFeature{"avx2",
-                      {"+avx", "+avx2", "+fxsr", "+mmx", "+popcnt", "+sse", "+sse2", "+sse3",
-                       "+sse4.1", "+sse4.2", "+ssse3", "+x87", "+xsave"}},
-        TargetFeature{"avx",
-                      {"+avx", "+fxsr", "+mmx", "+popcnt", "+sse", "+sse2", "+sse3", "+sse4.1",
-                       "+sse4.2", "+ssse3", "+x87", "+xsave"}},
-        TargetFeature{"sse4.2",
-                      {"+fxsr", "+mmx", "+popcnt", "+sse", "+sse2", "+sse3", "+sse4.1", "+sse4.2",
-                       "+ssse3", "+x87"}}};
-constexpr auto NUM_TARGETS = KNOWN_FEATURES.size();
+constexpr auto is64Bit(TargetID ID) -> bool {
+  switch (ID) {
+  case TargetID::AMDFAM10H_BARCELONA:
+  case TargetID::AMDFAM10H_SHANGHAI:
+  case TargetID::AMDFAM10H_ISTANBUL:
+    return false;
+  default:
+    return true;
+  }
+}
 
-using TargetFeatureFunctionPair =
-    std::pair<std::reference_wrapper<const TargetFeature>, Function *>;
+#ifndef NDEBUG
+void Dump(Value &V) { V.print(outs()); }
+void Dump(Module &M) { M.print(outs(), nullptr); }
+#endif
 
 class TargetClone : public ModulePass {
 public:
@@ -76,12 +97,14 @@ public:
 
   auto runOnModule(Module &M) -> bool override {
     for (auto *F : getClonableFunctions(M)) {
-      SmallVector<TargetFeatureFunctionPair, NUM_TARGETS> TargetFunctions;
+      SmallVector<TargetFunctionPair, NUM_TARGETS> TargetFunctions;
 
-      for (const auto &Feature : KNOWN_FEATURES) {
-        auto &Cloned = cloneFunction(*F, Feature.Name);
-        addTargetAttribute(Cloned, Feature);
-        TargetFunctions.push_back({Feature, &Cloned});
+      for (const auto &Target : TARGETS) {
+        if (is64Bit(Target.ID)) {
+          auto &Cloned = cloneFunction(*F, to_strref(Target.Name));
+          addTargetAttribute(Cloned, Target);
+          TargetFunctions.push_back({Target, &Cloned});
+        }
       }
 
       auto &Default = cloneFunction(*F, "default");
@@ -95,6 +118,135 @@ public:
   }
 
 private:
+  static void dispatchTargetFunctions(Function &F, ArrayRef<TargetFunctionPair> TargetFunctions,
+                                      Function &Default) {
+    for (auto BB = F.begin(); BB != F.end();) {
+      BB = BB->eraseFromParent();
+    }
+
+    auto &C = F.getContext();
+    IRBuilder<> Builder(C);
+    auto *BB = BasicBlock::Create(C, "entry", &F);
+    Builder.SetInsertPoint(BB);
+
+    emitMultiVersionResolver(F, Builder, TargetFunctions, Default);
+  }
+
+  static void addTargetAttribute(Function &F, const Target &T) {
+    auto Attrs = F.getAttributes().getFnAttributes();
+    auto &C = F.getContext();
+    Attrs = Attrs.removeAttribute(C, to_strref(TARGET_CPU))
+                .addAttribute(C, to_strref(TARGET_CPU), to_strref(T.Name));
+    F.setAttributes(F.getAttributes().addAttributes(C, AttributeList::FunctionIndex, Attrs));
+
+    SmallSet<StringRef, TYPICAL_FEATURE_COUNT> Features;
+
+    // Add features present already
+    SmallVector<StringRef, TYPICAL_FEATURE_COUNT> FeaturesVec;
+    Attrs.getAttribute(to_strref(TARGET_FEATURES))
+        .getValueAsString()
+        .split(FeaturesVec, ",", -1, false);
+    Features.insert(FeaturesVec.begin(), FeaturesVec.end());
+
+    // Add features from Target
+    FeaturesVec.clear();
+    getCPUSpecificFeatures(T.Name, FeaturesVec);
+    Features.insert(FeaturesVec.begin(), FeaturesVec.end());
+
+    // Concat all feature strings and add to Attributes
+    SmallString<TYPICAL_FEATURES_LEN> TargetFeatures;
+    for (const auto &F : Features) {
+      TargetFeatures += F;
+      TargetFeatures += ",";
+    }
+    // Pop trailing ','
+    TargetFeatures.pop_back();
+
+    Attrs = Attrs.removeAttribute(C, to_strref(TARGET_FEATURES))
+                .addAttribute(C, to_strref(TARGET_FEATURES), TargetFeatures);
+    F.setAttributes(F.getAttributes().addAttributes(C, AttributeList::FunctionIndex, Attrs));
+  }
+
+  static auto cloneFunction(Function &Src, StringRef Suffix) -> Function & {
+    auto *M = Src.getParent();
+    SmallString<TEMP_STR_LEN> Name = Src.getName();
+    auto *Type = Src.getFunctionType();
+    auto Attrs = Src.getAttributes();
+    auto &Dst = *cast<Function>(M->getOrInsertFunction((Name += ".") += Suffix, Type, Attrs));
+
+    SmallVector<ReturnInst *, 4> Returns;
+    ValueToValueMapTy VMap;
+
+    for (auto SrcArg = Src.arg_begin(), DstArg = Dst.arg_begin(); SrcArg != Src.arg_end();
+         ++SrcArg, ++DstArg) {
+      VMap[&*SrcArg] = DstArg;
+    }
+
+    CloneFunctionInto(&Dst, &Src, VMap, true, Returns);
+    Dst.setLinkage(GlobalValue::InternalLinkage);
+    return Dst;
+  }
+
+  // Based on http://bholt.org/posts/llvm-quick-tricks.html
+  using FunctionSet = SmallPtrSet<Function *, 4>;
+  static auto getClonableFunctions(const Module &M) -> FunctionSet {
+    FunctionSet Functions;
+    if (const auto *global_annos = M.getNamedGlobal("llvm.global.annotations")) {
+      auto *AnnoArray = cast<ConstantArray>(global_annos->getOperand(0));
+      for (int i = 0; i < AnnoArray->getNumOperands(); i++) {
+        auto *e = cast<ConstantStruct>(AnnoArray->getOperand(i));
+
+        if (auto *F = dyn_cast<Function>(e->getOperand(0)->getOperand(0))) {
+          auto *Anno = cast<ConstantDataArray>(
+              cast<GlobalVariable>(e->getOperand(1)->getOperand(0))->getOperand(0));
+          if (Anno->getRawDataValues().startswith(to_strref(TARGET_CLONE)))
+            Functions.insert(F);
+        }
+      }
+    }
+
+    return Functions;
+  }
+
+  static void emitMultiVersionResolver(Function &F, IRBuilder<> &Builder,
+                                       ArrayRef<TargetFunctionPair> TargetFunctions,
+                                       Function &Default) {
+    emitX86CpuInit(*F.getParent(), Builder);
+    uint64_t FeaturesMask = 0;
+
+    SmallVector<Value *, 10> Args; // NOLINT
+    for (auto &&Arg : F.args()) {
+      Args.push_back(&cast<Value>(Arg));
+    }
+
+    auto CallVariant = [&](Function *Func) {
+      if (F.getReturnType()->isVoidTy()) {
+        Builder.CreateCall(Func, Args);
+        Builder.CreateRetVoid();
+      } else {
+        auto *Ret = Builder.CreateCall(Func, Args);
+        Ret->setTailCallKind(CallInst::TCK_Tail);
+        Builder.CreateRet(Ret);
+      }
+    };
+
+    for (auto &&TargetFunction : TargetFunctions) {
+      auto &Cond = emitX86CpuIs(*F.getParent(), Builder, TargetFunction.first.get().Name);
+      auto *Call = BasicBlock::Create(F.getContext(),
+                                      to_strref(TargetFunction.first.get().Name) + ".call", &F);
+      auto *Cont = BasicBlock::Create(F.getContext(), "cont", &F);
+
+      Builder.CreateCondBr(&Cond, Call, Cont);
+
+      Builder.SetInsertPoint(Call);
+      CallVariant(TargetFunction.second);
+
+      Builder.SetInsertPoint(Cont);
+    }
+
+    CallVariant(&Default);
+  }
+
   static auto createGlobalFunction(Module &M, StringRef Name, FunctionType *FTy) -> Function & {
     auto &F = *cast<Function>(M.getOrInsertFunction(Name, FTy));
 
@@ -163,194 +315,58 @@ private:
     return *cast<Constant>(&CpuFeatures2);
   }
 
-  static auto getX86CpuSupportsMask(StringRef FeatureStr) -> uint64_t {
-    // Processor features and mapping to processor feature value.
-    unsigned Feature = StringSwitch<unsigned>(FeatureStr)
+  static void getCPUSpecificFeatures(std::string_view Name, SmallVectorImpl<StringRef> &Features) {
+    StringRef WholeList = StringSwitch<StringRef>(cpuSpecificNameDealias(Name))
     // NOLINTNEXTLINE
-#define X86_FEATURE_COMPAT(VAL, ENUM, STR) .Case(STR, VAL)
+#define CPU_SPECIFIC(NAME, MANGLING, FEATURES) .Case(NAME, FEATURES)
+#include <clang/Basic/X86Target.def>
+                              .Default("");
+    WholeList.split(Features, ',', /*MaxSplit=*/-1, /*KeepEmpty=*/false);
+  }
+
+  static auto cpuSpecificNameDealias(std::string_view Name) -> StringRef {
+    return StringSwitch<StringRef>(to_strref(Name))
+    // NOLINTNEXTLINE
+#define CPU_SPECIFIC_ALIAS(NEW_NAME, NAME) .Case(NEW_NAME, NAME)
+#include <clang/Basic/X86Target.def>
+        .Default(to_strref(Name));
+  }
+
+  static auto emitX86CpuIs(Module &M, IRBuilder<> &Builder, std::string_view CPUStr) -> Value & {
+    // Calculate the index needed to access the correct field based on the
+    // range. Also adjust the expected value.
+    unsigned Index;
+    unsigned Value;
+    std::tie(Index, Value) = StringSwitch<std::pair<unsigned, unsigned>>(to_strref(CPUStr))
+
+// NOLINTNEXTLINE
+#define X86_VENDOR(ENUM, STRING) .Case(STRING, {0u, static_cast<unsigned>(Vendor::ENUM)})
+// NOLINTNEXTLINE
+#define X86_CPU_TYPE_COMPAT_WITH_ALIAS(ARCHNAME, ENUM, STR, ALIAS)                                 \
+  .Cases(STR, ALIAS, {1u, static_cast<unsigned>(CPU::ENUM)})
+// NOLINTNEXTLINE
+#define X86_CPU_TYPE_COMPAT(ARCHNAME, ENUM, STR) .Case(STR, {1u, static_cast<unsigned>(CPU::ENUM)})
+// NOLINTNEXTLINE
+#define X86_CPU_SUBTYPE_COMPAT(ARCHNAME, ENUM, STR)                                                \
+  .Case(STR, {2u, static_cast<unsigned>(TargetID::ENUM)})
 #include <llvm/Support/X86TargetParser.def>
-        ;
-    return (1ULL << Feature);
-  }
+                                 .Default({0, 0});
+    assert(Value != 0 && "Invalid CPUStr passed to CpuIs");
 
-  static auto emitX86CpuSupportsMask(Module &M, IRBuilder<> &Builder, uint64_t FeaturesMask)
-      -> Value & {
-    uint32_t Features1 = Lo_32(FeaturesMask);
-    uint32_t Features2 = Hi_32(FeaturesMask);
-
+    // Grab the appropriate field from __cpu_model.
+    auto &CpuModel = getCpuModel(M, Builder);
     auto *Int32Ty = Builder.getInt32Ty();
-    Value *BitsetLo = Builder.getInt32(0);
-    Value *BitsetHi = Builder.getInt32(0);
+    std::array<llvm::Value *, 2> Idxs = {ConstantInt::get(Int32Ty, 0),
+                                         ConstantInt::get(Int32Ty, Index)};
+    llvm::Value *CpuValue =
+        Builder.CreateGEP(CpuModel.getType()->getPointerElementType(), &CpuModel, Idxs);
+    CpuValue = Builder.CreateAlignedLoad(CpuValue, 4);
 
-    if (Features1 != 0) {
-      auto &CpuModel = getCpuModel(M, Builder);
-
-      // Grab the first (0th) element from the field __cpu_features off of the
-      // global in the struct STy.
-      std::array<Value *, 3> Idxs = {Builder.getInt32(0), Builder.getInt32(3), Builder.getInt32(0)};
-      Value *CpuFeatures =
-          Builder.CreateGEP(CpuModel.getType()->getPointerElementType(), &CpuModel, Idxs);
-      auto *Features = Builder.CreateAlignedLoad(CpuFeatures, 4);
-
-      // Check the value of the bit corresponding to the feature requested.
-      auto *Mask = Builder.getInt32(Features1);
-      BitsetLo = Builder.CreateAnd(Features, Mask);
-    }
-
-    if (Features2 != 0) {
-      auto &CpuFeatures2 = getCpuFeatures2(M, Builder);
-      auto *Features = Builder.CreateAlignedLoad(&CpuFeatures2, 4);
-
-      // Check the value of the bit corresponding to the feature requested.
-      auto *Mask = Builder.getInt32(Features2);
-      BitsetHi = Builder.CreateAnd(Features, Mask);
-    }
-
-    constexpr auto INT32_BITS = 32;
-    BitsetHi = Builder.CreateZExt(BitsetHi, Builder.getInt64Ty());
-    BitsetHi = Builder.CreateShl(BitsetHi, Builder.getInt32(INT32_BITS));
-    BitsetLo = Builder.CreateZExt(BitsetLo, Builder.getInt64Ty());
-
-    return *Builder.CreateOr(BitsetHi, BitsetLo);
+    // Check the value of the field against the requested value.
+    return *Builder.CreateICmpEQ(CpuValue, ConstantInt::get(Int32Ty, Value));
   }
-
-  static void emitMultiVersionResolver(Function &F, IRBuilder<> &Builder,
-                                       ArrayRef<TargetFeatureFunctionPair> TargetFunctions,
-                                       Function &Default) {
-    emitX86CpuInit(*F.getParent(), Builder);
-    uint64_t FeaturesMask = 0;
-    SmallVector<uint64_t, NUM_TARGETS> FeatureMasks;
-    for (const auto &P : TargetFunctions) {
-      auto Mask = getX86CpuSupportsMask(P.first.get().Name);
-      FeaturesMask |= Mask;
-      FeatureMasks.push_back(Mask);
-    }
-    auto &FeaturesMaskRT = emitX86CpuSupportsMask(*F.getParent(), Builder, FeaturesMask);
-
-    SmallVector<Value *, 10> Args; // NOLINT
-    for (auto &&Arg : F.args()) {
-      Args.push_back(&cast<Value>(Arg));
-    }
-
-    auto CallVariant = [&](Function *Func) {
-      if (F.getReturnType()->isVoidTy()) {
-        Builder.CreateCall(Func, Args);
-        Builder.CreateRetVoid();
-      } else {
-        auto *Ret = Builder.CreateCall(Func, Args);
-        Ret->setTailCallKind(CallInst::TCK_Tail);
-        Builder.CreateRet(Ret);
-      }
-    };
-
-    for (auto [FeatureMask, TargetFunction] : zip(FeatureMasks, TargetFunctions)) {
-      auto *Cond = Builder.CreateAnd(&FeaturesMaskRT, Builder.getInt64(FeatureMask));
-      auto *Call =
-          BasicBlock::Create(F.getContext(), TargetFunction.first.get().Name + ".call", &F);
-      auto *Cont = BasicBlock::Create(F.getContext(), "cont", &F);
-
-      Builder.CreateCondBr(Builder.CreateICmpNE(Cond, Builder.getInt64(0)), Call, Cont);
-
-      Builder.SetInsertPoint(Call);
-      CallVariant(TargetFunction.second);
-
-      Builder.SetInsertPoint(Cont);
-    }
-
-    CallVariant(&Default);
-  }
-
-  static void dispatchTargetFunctions(Function &F,
-                                      ArrayRef<TargetFeatureFunctionPair> TargetFunctions,
-                                      Function &Default) {
-    for (auto BB = F.begin(); BB != F.end();) {
-      BB = BB->eraseFromParent();
-    }
-
-    auto &C = F.getContext();
-    IRBuilder<> Builder(C);
-    auto *BB = BasicBlock::Create(C, "entry", &F);
-    Builder.SetInsertPoint(BB);
-
-    emitMultiVersionResolver(F, Builder, TargetFunctions, Default);
-  }
-
-  static void addTargetAttribute(Function &F, const TargetFeature &TF) {
-    if (TF.Name == "default")
-      return;
-
-    SmallSet<StringRef, TYPICAL_FEATURE_COUNT> Features;
-
-    // Add features present already
-    SmallVector<StringRef, TYPICAL_FEATURE_COUNT> ExistingFeatures;
-    auto Attrs = F.getAttributes().getFnAttributes();
-    Attrs.getAttribute(TARGET_FEATURES).getValueAsString().split(ExistingFeatures, ",", -1, false);
-    Features.insert(ExistingFeatures.begin(), ExistingFeatures.end());
-
-    // Add features from Target
-    Features.insert(TF.Value.begin(), TF.Value.end());
-
-    // Concat all feature strings and add to Attributes
-    SmallString<TYPICAL_FEATURES_LEN> TargetFeatures;
-    for (const auto &F : Features) {
-      TargetFeatures += F;
-      TargetFeatures += ",";
-    }
-    // Pop trailing ',,'
-    TargetFeatures.resize(TargetFeatures.size() - 2);
-
-    auto &C = F.getContext();
-    Attrs =
-        Attrs.removeAttribute(C, TARGET_FEATURES).addAttribute(C, TARGET_FEATURES, TargetFeatures);
-    F.setAttributes(F.getAttributes().addAttributes(C, AttributeList::FunctionIndex, Attrs));
-  }
-
-  static auto cloneFunction(Function &Src, StringRef Suffix) -> Function & {
-    auto *M = Src.getParent();
-    SmallString<TEMP_STR_LEN> Name = Src.getName();
-    auto *Type = Src.getFunctionType();
-    auto Attrs = Src.getAttributes();
-    auto &Dst = *cast<Function>(M->getOrInsertFunction((Name += ".") += Suffix, Type, Attrs));
-
-    SmallVector<ReturnInst *, 4> Returns;
-    ValueToValueMapTy VMap;
-
-    for (auto SrcArg = Src.arg_begin(), DstArg = Dst.arg_begin(); SrcArg != Src.arg_end();
-         ++SrcArg, ++DstArg) {
-      VMap[&*SrcArg] = DstArg;
-    }
-
-    CloneFunctionInto(&Dst, &Src, VMap, true, Returns);
-    Dst.setLinkage(GlobalValue::InternalLinkage);
-    return Dst;
-  }
-
-  // Based on http://bholt.org/posts/llvm-quick-tricks.html
-  static auto getClonableFunctions(const Module &M) -> FunctionSet {
-    FunctionSet Functions;
-    if (const auto *global_annos = M.getNamedGlobal("llvm.global.annotations")) {
-      auto *AnnoArray = cast<ConstantArray>(global_annos->getOperand(0));
-      for (int i = 0; i < AnnoArray->getNumOperands(); i++) {
-        auto *e = cast<ConstantStruct>(AnnoArray->getOperand(i));
-
-        if (auto *F = dyn_cast<Function>(e->getOperand(0)->getOperand(0))) {
-          auto *Anno = cast<ConstantDataArray>(
-              cast<GlobalVariable>(e->getOperand(1)->getOperand(0))->getOperand(0));
-          if (Anno->getRawDataValues().startswith(TARGET_CLONE))
-            Functions.insert(F);
-        }
-      }
-    }
-
-    return Functions;
-  }
-
-  static void emitTrapCall(Module &M, IRBuilder<> &Builder) {
-    Builder.CreateCall(Intrinsic::getDeclaration(&M, Intrinsic::trap, None));
-  }
-
-}; // end of struct Hello
-} // end of anonymous namespace
+};
+} // namespace
 
 // NOLINTNEXTLINE
 static RegisterPass<TargetClone> X("target-clone", "Target Clone Pass");
--- test/test.c
+++ test/test.c
@@ -1,30 +1,20 @@
-int min(int a, int b) { return a < b ? a : b; }
-int max(int a, int b) { return a > b ? a : b; }
+#include <inttypes.h>
 
-__attribute__((annotate("targetclone"))) void my_min(int *a, const int *b, const int *c, int n) {
-  int i;
-  for (i = 0; i < n; i++) {
-    a[i] = (b[i] < c[i]) ? b[i] : c[i];
+typedef uint64_t uint64;
+typedef int64_t int64;
+typedef uint64 rowcnt_t;
+typedef uint64 *selvec_t;
+typedef int64 index_t;
+
+__attribute__((annotate("targetclone"))) rowcnt_t
+SelectValidBuckets(rowcnt_t selrows, selvec_t srcSelvec, selvec_t dstSelvec, uint64 *entryIdArr) {
+  rowcnt_t numValid = 0;
+
+  for (index_t i = 0; i < selrows; i++) {
+    index_t ind = srcSelvec[i];
+    dstSelvec[numValid] = ind;
+    numValid += (entryIdArr[ind] != 0);
   }
+
+  return numValid;
 }
-
-// #include <inttypes.h>
-
-// typedef uint64_t uint64;
-// typedef int64_t int64;
-// typedef uint64 rowcnt_t;
-// typedef uint64 *selvec_t;
-// typedef int64 index_t;
-
-// rowcnt_t SelectValidBuckets(rowcnt_t selrows, selvec_t srcSelvec, selvec_t dstSelvec,
-//                             uint64 *entryIdArr) {
-//     rowcnt_t numValid = 0;
-
-//     for (index_t i = 0; i < selrows; i++) {
-//         index_t ind = srcSelvec[i];
-//         dstSelvec[numValid] = ind;
-//         numValid += (entryIdArr[ind] != 0);
-//     }
-
-//     return numValid;
-// }
